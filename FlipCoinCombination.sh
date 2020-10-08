5cb3619d60f094fc2dafd64eaaff7ad086986b3c
poojakakuru <poojareddykakuru@gmail.com>
2020-10-08 12:21:58 +0530

    Doublet combination is done

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -36,3 +36,38 @@ perhead=`expr $perhead/25`;
 pertail=`expr 100 - $perhead`;
 echo "percentage of heads is $perhead"
 echo "percentage of tails is $pertail"
+
+declare -A coin;
+coin[headhead]=0;
+coin[tailtail]=0;
+coin[headtail]=0;
+count=0;
+
+addtoDictionary(){
+if [ $1 -eq 0 ]
+then
+   coin[headhead]=`expr ${coin[headhead]}+1`;
+elif [ $1 -eq 1 ]
+then
+   coin[tailtail]=`expr ${coin[tailtail]}+1`;
+else
+   coin[headtail]=`expr ${coin[headtail]}+1`;
+fi
+}
+
+while [ $count -le 25 ]
+do
+flip=$(( RANDOM%3 ));
+addtoDictionary $flip;
+((++count))
+done
+
+perhead=`expr ${coin[headheaf]}\*100`;
+perhead=`expr $perhead /25`;
+pertail=`expr ${coin[tailtail]}\*100`;
+pertail=`expr $pertail/25`;
+perheadtail=`expr ${coin[headtail]}\*100`;
+perheadtail=`expr $perheadtail/25`;
+echo "Percentage of headhead is $perhead"
+echo "Percentage of tailtail is $pertail"
+echo "Percentage of headtail is $perheadtail"
